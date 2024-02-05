676856877d102fd2e301daab518493763c320daf
Phanna16832 <your.email@example.com>
2024-02-05 15:26:08 +0700

    update

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//search code
 document.addEventListener("DOMContentLoaded", function () {
     var searchForm = document.getElementById('searchForm');
     searchForm.addEventListener('submit', performSearch);
@@ -11,15 +10,14 @@ function performSearch(event) {
 
     if (searchInputValue.trim() !== '') {
         var data = [
-            { title: "address , អាស័យដ្ឋាន , 收货地址", file:"address.html"},
-            { title: "ស្បែកជើង  , shoe , 鞋子", img:"pr1.jpg"},
-            { title: "", img: "tb.jpg"},
-            { title: "", text: " ", file: "" }  
+            { title: "address , អាស័យដ្ឋាន , 收货地址", file: "address.html" },
+            { title: "ស្បែកជើង  , shoe , 鞋子", img: "pr1.jpg", text: "lorem" },
+            { title: "", img: "tb.jpg" },
+            { title: "", text: " ", file: "" }
             // Add more results as needed
         ];
 
         var filteredResults = data.filter(result => result.title.toLowerCase().includes(searchInputValue));
-
         displayResults(filteredResults);
     } else {
         clearResults();
@@ -37,21 +35,29 @@ function displayResults(results) {
         if (result.img) {
             var imgElement = document.createElement('img');
             imgElement.src = result.img;
+            imgElement.style.width = "170px";
+            imgElement.style.marginBottom = "2%";
+            imgElement.style.margin = "0 auto"; // Center horizontally
             resultItem.appendChild(imgElement);
         }
 
         if (result.text) {
             var textElement = document.createElement('p');
+            textElement.style.color = "pink";
             textElement.textContent = result.text;
             resultItem.appendChild(textElement);
         }
 
-        var detailsLink = document.createElement('a');
-        detailsLink.href = result.file;
-        detailsLink.textContent = 'View Details';
-        detailsLink.classList.add('details-link');
-        resultItem.appendChild(detailsLink);
+        if (result.file) {
+            var detailsLink = document.createElement('a');
+            detailsLink.href = result.file;
+            detailsLink.textContent = 'View Details';
+            detailsLink.classList.add('details-link');
+            resultItem.appendChild(detailsLink);
+        }
 
+        // Applying global styles to the entire resultItem
+        resultItem.style.color = "blue";
         searchResultsContainer.appendChild(resultItem);
     });
 }
