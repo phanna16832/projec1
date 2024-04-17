fc9884ffeb36bab4ed091e1012d75d1116f9f13b
phanna16832 <149858943+phanna16832@users.noreply.github.com>
2024-04-17 05:30:14 +0000

    1

--- script.js
+++ script.js
@@ -32,9 +32,28 @@ function addToCart(productId) {
 }
 
 // Function to handle checkout
-function checkout() {
-    alert('Checkout functionality will be implemented in the future.');
+function requestNotificationPermission() {
+    // Check if the browser supports notifications
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    } else {
+        // Request permission from the user
+        Notification.requestPermission().then(function (permission) {
+            if (permission === "granted") {
+                showNotification("Thank for purchasing in the app");
+            }
+        });
+    }
 }
+
+function showNotification(title, body) {
+    // Check if notifications are supported and permission is granted
+    if (Notification.permission === "granted") {
+        // Create and show the notification
+        var notification = new Notification(title, { body: body });
+    }
+}
+
 
 // Initialize
 displayProducts();
